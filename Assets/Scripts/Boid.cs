b31f889b7e0b295eb7d5f94d3ce71abf9ec495e3
JMulligan94 <j.mulligan94@hotmail.co.uk>
2020-05-23 23:54:09 +0100

    Crude implementation

--- Assets/Scripts/Boid.cs
+++ Assets/Scripts/Boid.cs
@@ -4,30 +4,69 @@ using UnityEngine;
 
 public class Boid : MonoBehaviour
 {
-	[Range(0.5f, 5.0f)]
-	public float m_speed = 1.0f; // 1 unit / sec
+	private Vector3 m_velocity;
+	private Vector3 m_upAxis;
 
-	public bool m_drawAxis = false;
+	private List<Boid> m_neighbours = new List<Boid>();
 
-	// Start is called before the first frame update
-	void Start()
+	public void DrawDebugAxis()
 	{
-		
+		Debug.DrawRay( transform.position, transform.forward, Color.blue );
+		Debug.DrawRay( transform.position, transform.up, Color.green );
+		Debug.DrawRay( transform.position, transform.right, Color.red );
 	}
 
-	// Update is called once per frame
-	void Update()
+	public void UpdatePosition()
 	{
-		float movementDelta = m_speed * Time.deltaTime;
-		Vector3 newPosition = transform.localPosition + ( transform.forward * movementDelta );
+		transform.position += m_velocity * Time.fixedDeltaTime;
+		transform.position = BoidManager.Instance.GetWrappedPosition( transform.position );
 
-		if ( m_drawAxis )
+		Quaternion fromRotation = transform.rotation;
+		Quaternion toRotation = Quaternion.LookRotation( m_velocity, m_upAxis );
+		transform.rotation = Quaternion.RotateTowards( fromRotation, toRotation, 500 * Time.fixedDeltaTime );
+	}
+
+	public Vector3 GetBoidVelocity()
+	{
+		return m_velocity;
+	}
+
+	public void SetBoidVelocity( Vector3 velocity )
+	{
+		m_velocity = velocity;
+		if ( m_velocity.sqrMagnitude > BoidManager.Instance.m_maxSpeed * BoidManager.Instance.m_maxSpeed )
 		{
-			Debug.DrawRay( transform.position, transform.forward, Color.blue );
-			Debug.DrawRay( transform.position, transform.up, Color.green );
-			Debug.DrawRay( transform.position, transform.right, Color.red );
+			m_velocity = m_velocity.normalized * BoidManager.Instance.m_maxSpeed;
+		}
+		else if ( m_velocity.sqrMagnitude < BoidManager.Instance.m_minSpeed * BoidManager.Instance.m_minSpeed )
+		{
+			m_velocity = m_velocity.normalized * BoidManager.Instance.m_minSpeed;
 		}
 
-		transform.localPosition = BoidManager.Instance.GetWrappedPosition( newPosition );
+		Vector3 normVelocity = m_velocity.normalized;
+		m_upAxis = Vector3.Cross( m_velocity, new Vector3( normVelocity.y, normVelocity.z, normVelocity.x ) );
+	}
+
+	public void AddNeighbour( Boid neighbour )
+	{
+		m_neighbours.Add( neighbour );
+	}
+
+	public void ClearNeighbours()
+	{
+		m_neighbours.Clear();
+	}
+
+	public int GetNeighbourCount()
+	{
+		return m_neighbours.Count;
+	}
+
+	public IEnumerable<Boid> IterateNeighbours()
+	{
+		foreach ( Boid neighbour in m_neighbours )
+		{
+			yield return neighbour;
+		}
 	}
 }
--- Assets/Scripts/BoidManager.cs
+++ Assets/Scripts/BoidManager.cs
@@ -7,38 +7,235 @@ public class BoidManager : MonoBehaviour
 	public static BoidManager Instance;
 
 	public BoxCollider m_bounds;
-
-	[Range( 2, 40 )]
+	
 	public int m_numBoids = 10;
+	
+	[Header("Boid Settings")]	
+	public bool m_drawBoidAxis = false;
 
+	public float m_neighbourRadius = 1.0f;
+	
 	public GameObject m_boidPrefab;
+	public bool m_updatePosition = true;
+
+	public float m_minSpeed = 0.1f;
+	public float m_maxSpeed = 2.0f;
+
+	[Header( "Separation Settings" )]
+	public bool m_enableSeparation = true;
+	public bool m_drawSeparationDebugRays = true;
+	[Tooltip( "The minimum amount of distance between any two boids" )]
+	public float m_separationValue = 1.55f;
+	[Tooltip( "How strong the force of separation is" )]
+	public float m_separationFactor = 0.05f;
+
+	[Header( "Alignment Settings" )]
+	public bool m_enableAlignment = true;
+	public bool m_drawAlignmentDebugRays = true;
+	public float m_alignmentFactor = 0.05f;
+
+	[Header( "Cohesion Settings" )]
+	public bool m_enableCohesion = true;
+	public bool m_drawCohesionDebugRays = true;
+	public float m_cohesionFactor = 0.05f;
 
 	Boid[] m_boids;
 	Vector3 m_minBounds;
 	Vector3 m_maxBounds;
 
-	private void OnEnable()
-	{
-		if ( Instance == null )
-		{
-			Instance = this;
-		}
-	}
-
 	// Start is called before the first frame update
 	void Start()
 	{
+		if ( Instance == null )
+			Instance = this;
+
 		m_minBounds = m_bounds.transform.position + m_bounds.center - ( m_bounds.size / 2.0f );
 		m_maxBounds = m_bounds.transform.position + m_bounds.center + ( m_bounds.size / 2.0f );
 
 		GenerateBoids();
 	}
-
-	// Update is called once per frame
-	void Update()
+	
+	void FixedUpdate()
 	{
+		// Find boids neighbours
+		PerformNeighbourSearch();
+
+		if ( !m_updatePosition )
+			return;
 		
+		for ( int i = 0; i < m_boids.Length; ++i )
+		{
+			Boid boid = m_boids[ i ];
+
+			Vector3 newVelocity = Vector3.zero;
+
+			// 1) SEPARATION
+			Vector3 separation = Vector3.zero;
+			if ( m_enableSeparation )
+			{
+				separation = CalculateSeparation( i );
+				if ( m_drawSeparationDebugRays
+					&& separation.sqrMagnitude > 0.0f )
+				{
+					DrawRedRay( boid, separation );
+				}
+
+				newVelocity += separation * m_separationFactor;
+			}
+
+			// 2) ALIGNMENT
+			Vector3 alignment = Vector3.zero;
+			if ( m_enableAlignment )
+			{
+				alignment = CalculateAlignment( i );
+				if ( m_drawAlignmentDebugRays 
+					&& alignment.sqrMagnitude > 0.0f )
+				{
+					DrawRay( boid, alignment, Color.blue );
+				}
+
+				newVelocity += alignment * m_alignmentFactor;
+			}
+
+			// 3) COHESION
+			Vector3 cohesion = Vector3.zero;
+			if ( m_enableCohesion )
+			{
+				cohesion = CalculateCohesion( i );
+				if ( m_drawCohesionDebugRays
+					&& cohesion.sqrMagnitude > 0.0f )
+				{
+					DrawRay( boid, cohesion, Color.green );
+				}
+
+				newVelocity += cohesion * m_cohesionFactor;
+			}
+
+
+			if ( m_drawBoidAxis )
+			{
+				boid.DrawDebugAxis();
+			}
+				
+			boid.SetBoidVelocity( newVelocity );
+			boid.UpdatePosition();
+			boid.ClearNeighbours();
+		}
 	}
+
+	private void PerformNeighbourSearch()
+	{
+		float neighbourRadSqr = m_neighbourRadius * m_neighbourRadius;
+		for ( int i = 0; i < m_numBoids; ++i )
+		{
+			Boid boid = m_boids[ i ];
+			Vector3 boidPos = boid.transform.position;
+			for ( int j = i + 1; j < m_numBoids; ++j )
+			{
+				float distanceSquared = ( m_boids[ j ].transform.position - boidPos ).sqrMagnitude;
+				if ( distanceSquared <= neighbourRadSqr )
+				{
+					m_boids[ i ].AddNeighbour( m_boids[ j ] );
+					m_boids[ j ].AddNeighbour( m_boids[ i ] );
+				}
+			}
+		}
+	}
+
+	// SEPARATION: Steering to avoid colliding or crowing with other flockmates
+	private Vector3 CalculateSeparation( int boidIndex )
+	{
+		Boid boid = m_boids[ boidIndex ];
+		if ( boid.GetNeighbourCount() == 0 )
+			return Vector3.zero;
+
+		Vector3 separationForce = Vector3.zero;
+
+		Vector3 boidPosition = boid.transform.position;
+		float separationDistSqr = m_separationValue * m_separationValue;
+
+		foreach ( Boid neighbour in boid.IterateNeighbours() )
+		{
+			Vector3 difference = boidPosition - neighbour.transform.position;
+			float distanceSqr = difference.sqrMagnitude;
+			if ( distanceSqr < separationDistSqr )
+			{
+				// Needs separating
+				Vector3 separate = difference;
+				separate.Normalize();
+				separate = separate / distanceSqr;
+				separationForce += separate; 
+			}
+		}
+
+		return separationForce;
+	}
+
+	// ALIGNMENT: Steering to move with the average heading of flockmates
+	private Vector3 CalculateAlignment( int boidIndex )
+	{
+		Boid boid = m_boids[ boidIndex ];
+		if ( boid.GetNeighbourCount() == 0 )
+			return Vector3.zero;
+
+		Vector3 alignmentForce = Vector3.zero;
+		Vector3 sumHeading = Vector3.zero;
+		foreach ( Boid neighbour in boid.IterateNeighbours() )
+		{
+			sumHeading = neighbour.GetBoidVelocity().normalized;
+		}
+
+		alignmentForce = sumHeading / boid.GetNeighbourCount();
+		return alignmentForce;
+	}
+
+	// COHESION: Steering to move towards the average position of flockmates
+	private Vector3 CalculateCohesion( int boidIndex )
+	{
+		Boid boid = m_boids[ boidIndex ];
+		if ( boid.GetNeighbourCount() == 0 )
+			return Vector3.zero;
+
+		Vector3 cohesionForce = Vector3.zero;
+		Vector3 averagePosition = Vector3.zero;
+		foreach ( Boid neighbour in boid.IterateNeighbours() )
+		{
+			averagePosition += neighbour.transform.position;
+		}
+
+		averagePosition /= boid.GetNeighbourCount();
+
+		cohesionForce = averagePosition - m_boids[ boidIndex ].transform.position;
+		return cohesionForce;
+	}
+
+	private void GenerateBoids()
+	{
+		m_boids = new Boid[ m_numBoids ];
+		for ( int i = 0; i < m_numBoids; ++i )
+		{
+			Vector3 randomPosition = new Vector3( Random.Range( m_minBounds.x, m_maxBounds.x ),
+								Random.Range( m_minBounds.y, m_maxBounds.y ),
+								Random.Range( m_minBounds.z, m_maxBounds.z ) );
+
+			Quaternion randomRotation = Quaternion.Euler( Random.Range( 0, 360 ), Random.Range( 0, 360 ), Random.Range( 0, 360 ) );
+
+			GameObject boidObj = Instantiate( m_boidPrefab, randomPosition, randomRotation, transform );
+			boidObj.name = "Boid_" + i;
+			m_boids[ i ] = boidObj.GetComponent<Boid>();
+		}
+	}
+
+	private void DrawRedRay( Boid boid, Vector3 direction )
+	{
+		DrawRay( boid, direction, Color.red );
+	}
+
+	private void DrawRay( Boid boid, Vector3 direction, Color colour )
+	{
+		Debug.DrawRay( boid.transform.position, direction, colour );
+	}
+
 
 	public Vector3 GetWrappedPosition( Vector3 worldPosition )
 	{
@@ -51,7 +248,7 @@ public class BoidManager : MonoBehaviour
 			dist = m_minBounds.x - wrappedPosition.x;
 			wrappedPosition.x = m_maxBounds.x - dist;
 		}
-		else if (wrappedPosition.x > m_maxBounds.x)
+		else if ( wrappedPosition.x > m_maxBounds.x )
 		{
 			dist = wrappedPosition.x - m_maxBounds.x;
 			wrappedPosition.x = m_minBounds.x + dist;
@@ -82,22 +279,5 @@ public class BoidManager : MonoBehaviour
 		}
 
 		return wrappedPosition;
-	}
-
-	private void GenerateBoids()
-	{
-		m_boids = new Boid[ m_numBoids ];
-		for ( int i = 0; i < m_numBoids; ++i )
-		{
-			Vector3 randomPosition = new Vector3( Random.Range( m_minBounds.x, m_maxBounds.x ),
-								Random.Range( m_minBounds.y, m_maxBounds.y ),
-								Random.Range( m_minBounds.z, m_maxBounds.z ) );
-
-			Quaternion randomRotation = Quaternion.Euler( Random.Range( 0, 360 ), Random.Range( 0, 360 ), Random.Range( 0, 360 ) );
-
-			GameObject boidObj = Instantiate( m_boidPrefab, randomPosition, randomRotation, transform );
-			boidObj.name = "Boid_" + i;
-			m_boids[ i ] = boidObj.GetComponent<Boid>();
-		}
 	}
 }
